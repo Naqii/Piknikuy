c4085593c45403626236d3bcbb93a0f48193224a
Naqi <56961965+Naqii@users.noreply.github.com>
2021-12-07 14:36:27 +0700

    Piknikuy Apps 0.1

---
+++ app/src/main/java/com/example/piknikuy/SplashScreen.kt
@@ -0,0 +1,25 @@
+package com.example.piknikuy
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, WAITING.toLong())
+    }
+
+    companion object {
+        const val WAITING = 2000
+    }
+}
