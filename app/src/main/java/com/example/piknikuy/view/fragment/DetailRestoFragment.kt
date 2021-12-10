705cc8a3ac258732e4b187dd0bb82996a20fb211
ardiansyahdimas <dimasar863@gmail.com>
2021-12-10 20:22:34 +0700

    Add DetailRestoFragment and ListRestoFragment

---
+++ app/src/main/java/com/example/piknikuy/view/fragment/DetailRestoFragment.kt
@@ -0,0 +1,25 @@
+package com.example.piknikuy.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.example.piknikuy.R
+
+class DetailRestoFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail_resto, container, false)
+
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as AppCompatActivity).supportActionBar?.title = "Detail Restaurant"
+    }
+
+}
