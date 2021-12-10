2bc0e1e15a8f029b3c6e7e9f28453dca7655283a
ardiansyahdimas <dimasar863@gmail.com>
2021-12-10 07:48:23 +0700

    no message

--- app/src/main/java/com/example/piknikuy/adapter/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/piknikuy/adapter/SectionsPagerAdapter.kt
@@ -3,8 +3,8 @@ package com.example.piknikuy.adapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.piknikuy.fragment.TopVisitorFragment
-import com.example.piknikuy.fragment.TrendFragment
+import com.example.piknikuy.view.fragment.TopVisitorFragment
+import com.example.piknikuy.view.fragment.TrendFragment
 
 class SectionsPagerAdapter (activity: AppCompatActivity) : FragmentStateAdapter(activity) {
     override fun getItemCount(): Int {
