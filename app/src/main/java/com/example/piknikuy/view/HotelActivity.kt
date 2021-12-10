2f028185a375a9a56b08f291fc2f0eb6cfbde630
ardiansyahdimas <dimasar863@gmail.com>
2021-12-10 20:18:59 +0700

    add Activity and Fragment (Hotel)

---
+++ app/src/main/java/com/example/piknikuy/view/HotelActivity.kt
@@ -0,0 +1,11 @@
+package com.example.piknikuy.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HotelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hotel)
+    }
+}
