1d2be7586d96b99df88c33f41bb63592b2b232ef
Naqi <56961965+Naqii@users.noreply.github.com>
2021-12-15 17:46:47 +0700

    Piknikuy Apps 0.6

--- app/src/main/java/com/example/piknikuy/view/MainActivity.kt
+++ app/src/main/java/com/example/piknikuy/view/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.example.piknikuy.view
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager2.widget.ViewPager2
 import com.example.piknikuy.R
 import com.example.piknikuy.adapter.SectionsPagerAdapter
 import com.example.piknikuy.databinding.ActivityMainBinding
+import com.example.piknikuy.setting.SettingActivity
+import com.example.piknikuy.setting.SettingPreferences
+import com.example.piknikuy.view.hotel.FavoriteHotel
 import com.example.piknikuy.view.hotel.HotelActivity
 import com.example.piknikuy.view.resto.RestoActivity
+import com.example.piknikuy.viewModel.SettingsViewModel
+import com.example.piknikuy.viewModel.ViewModelFactory
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
@@ -22,12 +34,18 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var activityMainBinding: ActivityMainBinding
 
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
 
+        supportActionBar?.title = getString(R.string.piknikuy)
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+
         btnMenu()
+        darkMode()
 
         val sectionsPagerAdapter = SectionsPagerAdapter(this)
         val viewPager: ViewPager2 = findViewById(R.id.view_pager)
@@ -39,6 +57,24 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.elevation = 0f
     }
 
+    private fun darkMode() {
+        val pref = SettingPreferences.getInstance(dataStore)
+        val settingViewModel = ViewModelProvider(this, ViewModelFactory(pref)).get(
+            SettingsViewModel::class.java
+        )
+        settingViewModel.getThemeSettings().observe(
+            this, { isDarkModeActive: Boolean ->
+                if (isDarkModeActive) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    return@observe
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    return@observe
+                }
+            }
+        )
+    }
+
     private fun btnMenu() {
         activityMainBinding.btnResto.setOnClickListener {
             val intent = Intent(this, RestoActivity::class.java)
@@ -47,6 +83,22 @@ class MainActivity : AppCompatActivity() {
         activityMainBinding.btnHotel.setOnClickListener {
             val intent = Intent(this, HotelActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.favorite -> {
+                val f = Intent(this, FavoriteHotel::class.java)
+                startActivity(f)
+                return true
+            }
+            R.id.setting -> {
+                val s = Intent(this, SettingActivity::class.java)
+                startActivity(s)
+                return true
+            }
+            else -> return true
         }
     }
 }
